7cd4b1caa45db941674a0066fcbb6dc67901f9a3
Diana-nyamai <dnyamai.dn@gmail.com>
2020-07-22 06:27:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import{ BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Router>
+  <div>
+    <ul>
+      <li><Link to="/home">Home</Link></li>
+      <li><Link to="/accounts">Accounts</Link></li>
+    </ul>
+    <hr/>
+   <Switch>
+   <Route exact path="/home"> 
+     <Home/>
+   </Route>
+   <Route path="/accounts"> 
+   <Accounts/>
+   </Route>
+   </Switch>
+    </div>
+</Router>
     </div>
   );
+}
+ 
+function Home() {
+  return(
+  <div>
+    <h2>home page</h2>
+  </div>
+  );
+}
+
+function Accounts(){
+  let {path,url} = useRouteMatch();
+  return(
+   <div>
+     <h3>My List Of Accounts</h3>
+     <ul>
+       <li><Link to={`${url}/netflix`}>Netflix</Link></li>
+       <li><Link to={`${url}/123movies`}>123movies</Link></li>
+       <li><Link to={`${url}/fmovies`}>fmovies</Link></li>
+     </ul>
+     <switch>
+       <Route exact path={path}>choose one your accounts</Route>
+       <Route path={`${path}/:id`}>
+         <Account/>
+       </Route>
+     </switch>
+   </div>
+  );
+}
+
+function Account(){
+  let {id} = useParams();
+  return(
+    <div>
+      <h4>{id}</h4>
+    </div>
+  )
 }
 
 export default App;
